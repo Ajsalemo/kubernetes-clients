07da3977b9a02ba55293b3865f239aa302f5bbbe
Anthony Salemo <ajssalemo@gmail.com>
2026-03-08 17:00:39 -0400

    added argparse and fastpi, trying to construct CLI and server usage

--- python/main.py
+++ python/main.py
@@ -1,9 +1,16 @@
-from kubernetes import config, client
+import argparse
+import logging
+import sys
+
+from fastapi import FastAPI
+from kubernetes import client, config
+
 from src.listallpods import listAllPods
-import logging, sys
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+app = FastAPI()
 
 # load the kubeconfig
 try:
@@ -13,15 +20,43 @@ try:
 except config.ConfigException as e:
     logger.error("Failed to load kubeconfig: %s", e)
     sys.exit(1)
-    
+
 # create the api client
 # pass this in to any functions
 v1 = client.CoreV1Api()
+# This program can be ran as a CLI tool or as a web server
+# If ran through a server like gunicorn/uvicorn, app the annotated @app routes will be invoked - main() will not be invoked
+# If ran through the CLI, main() will be invoked and the @app routes will not be invoked
+
+@app.get("/pods/list/all/{namespace}")
+def read_item(namespace: str = "default"):
+    listAllPods(v1, namespace)
+    return {"message": f"Listed all pods in namespace: {namespace}"}
+
 
 def main():
-    # list all pods in the cluster
-    listAllPods(v1)
+    # list all pods
+    pod_list_all = argparse.ArgumentParser(description="x")
+    pod_list_all.add_argument(
+        "pod", help="x"
+    )
+    pod_list_all.add_argument(
+        "list", help="x"
+    )
+    pod_list_all.add_argument(
+        "--namespace", type=str, default="default", help="Namespace to list pods from"
+    )
+    pod_list_all.add_argument(
+        "--all", type=bool, default=False, help="List all pods from the specified namespace. Namespace is 'default' if not specified."
+    )    
+    pod_list_all.add_argument(
+        "--watch", type=bool, default=False, help="Watch for changes in the pods"
+    )    
+    pod_list_all_args = pod_list_all.parse_args()
 
+    # list all pods in the cluster
+    if pod_list_all_args.pod == "pod" and pod_list_all_args.list == "list":
+        listAllPods(v1, pod_list_all_args.namespace, pod_list_all_args.watch, pod_list_all_args.all)
 
 if __name__ == "__main__":
     main()
--- python/src/listallpods.py
+++ python/src/listallpods.py
@@ -4,9 +4,11 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def listAllPods(v1):
-    logger.info(" Listing all pods in the cluster:")
-    pods = v1.list_pod_for_all_namespaces(watch=False)
+def listAllPods(v1, namespace, watch=False, all=False):
+    # List all pods in all namespaces
+    if all:
+        logger.info(f" Listing all pods in the namespace: {namespace}")
+        pods = v1.list_namespaced_pod(namespace, watch=watch)
     logger.info(f" Total pods found: {len(pods.items)}")
     for pod in pods.items:
         logger.info(f" {pod.metadata.namespace}\t{pod.metadata.name}")
