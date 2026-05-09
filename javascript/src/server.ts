e392e5ead18b6b03bf8349a661b008c4178e8614
Anthony Salemo <ajssalemo@gmail.com>
2026-05-09 10:22:12 -0400

    added a route for finding a pod in a specific namespace

--- javascript/src/server.ts
+++ javascript/src/server.ts
@@ -1,5 +1,5 @@
-import * as k8s from '@kubernetes/client-node';
-import Fastify, { FastifyInstance } from 'fastify';
+import * as k8s from "@kubernetes/client-node";
+import Fastify, { FastifyInstance } from "fastify";
 
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
@@ -10,22 +10,48 @@ const server: FastifyInstance = Fastify({
     logger: true
 });
 
-// GET route with query parameter validation
-server.get<{ Querystring: { namespace: string } }>('/pods/list/all', async (request, reply) => {
+// HTTP GET - list all pods in a namespace (default namespace is "default")
+// Request URL shape: /pods/list/all?namespace=<namespace>
+server.get<{ Querystring: { namespace: string } }>("/pods/list/all", async (request, _) => {
     let { namespace } = request.query;
     // If namespace is empty, default to "default"
     if (!namespace) {
         namespace = "default";
     }
+    // Line 26 { namespace } as .. - is syntax to fit the shape of the below interface that CoreV1ApiListNamespacedPodRequest is
+    // and which contains `namespace` 
+    // export interface CoreV1ApiListNamespacedPodRequest {
+    //     namespace: string;
+    //     ..other props
+    // }
     const n = { namespace } as k8s.CoreV1ApiListNamespacedPodRequest;
     const res = await k8sApi.listNamespacedPod(n);
     return { pods: res.items };
 });
 
+// HTTP GET - find a specific pod in a namespace
+// Request URL shape: /pod/get?pod=<podname>&namespace=<namespace>
+server.get<{ Querystring: { pod: string, namespace?: string } }>("/pod/get", async (request, response) => {
+    const { pod } = request.query;
+    let { namespace } = request.query;
+    // If no pod is provided then return an HTTP 400
+    if (!pod) {
+        return response.status(400).send({ error: "Pod name is required" });
+    }
+    // If namespace is empty, default to "default"
+    if (!namespace) {
+        namespace = "default";
+    }
+    // Same as line 20, fit the shape of the below interface that CoreV1ApiReadNamespacedPodRequest is
+    const p = { name: pod, namespace } as k8s.CoreV1ApiReadNamespacedPodRequest;
+    const getPod = await k8sApi.readNamespacedPod(p);
+    return { pod: getPod.spec };
+});
+
 // Start server
 const start = async () => {
     try {
-        await server.listen({ port: 3000, host: '0.0.0.0' });
+        await server.listen({ port: 3000, host: "0.0.0.0" });
         server.log.info(`Server running at http://0.0.0.0:3000`);
     } catch (err) {
         server.log.error(err);
@@ -33,4 +59,9 @@ const start = async () => {
     }
 };
 
-start();
+try {
+    start();
+} catch (err) {
+    server.log.error(err);
+    process.exit(1);
+}
