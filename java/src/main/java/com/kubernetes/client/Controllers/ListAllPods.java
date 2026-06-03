39341a95675c556f9422d3f71635e186e11f8cb9
Anthony Salemo <ajssalemo@gmail.com>
2026-06-02 21:40:49 -0400

    added a test endpoint to list all pods

---
+++ java/src/main/java/com/kubernetes/client/Controllers/ListAllPods.java
@@ -0,0 +1,30 @@
+package com.kubernetes.client.Controllers;
+
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.Config;
+
+@RestController
+public class ListAllPods {
+    @GetMapping("/pods/list/all")
+    public String listAllPods() throws IOException, ApiException {
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+
+        CoreV1Api api = new CoreV1Api();
+        V1PodList list = api.listNamespacedPod("kube-system", null, null, null, null, null, null, null, null, null, false);
+        for (V1Pod item : list.getItems()) {
+            System.out.println(item.getMetadata().getName());
+        }
+        return "List of all pods in the cluster";
+    }
+}
