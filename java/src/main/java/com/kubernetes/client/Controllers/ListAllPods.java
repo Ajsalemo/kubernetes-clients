6575fae669e83ea2e2b4f5f2aff53c7f88f2d942
Anthony Salemo <ajssalemo@gmail.com>
2026-06-05 22:26:24 -0400

    changed javas /pod/list/all to return json instead of a string of pods

--- java/src/main/java/com/kubernetes/client/Controllers/ListAllPods.java
+++ java/src/main/java/com/kubernetes/client/Controllers/ListAllPods.java
@@ -1,7 +1,10 @@
 package com.kubernetes.client.Controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,15 +19,17 @@ import io.kubernetes.client.util.Config;
 @RestController
 public class ListAllPods {
     @GetMapping("/pods/list/all")
-    public String listAllPods() throws IOException, ApiException {
+    public ResponseEntity<List<String>> listAllPods() throws IOException, ApiException {
         ApiClient client = Config.defaultClient();
+        List<String> podList = new ArrayList<>();
         Configuration.setDefaultApiClient(client);
 
         CoreV1Api api = new CoreV1Api();
         APIlistNamespacedPodRequest list = api.listNamespacedPod("kube-system");
         for (V1Pod item : list.execute().getItems()) {
             System.out.println(item.getMetadata().getName());
+            podList.add(item.getMetadata().getName());
         }
-        return "List of all pods in the cluster";
+        return ResponseEntity.ok(podList);
     }
 }
